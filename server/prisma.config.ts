f449defbcea88a3a396de139ab8684f369ffddd5
Tanner Carter <641339+tanner-c@users.noreply.github.com>
2025-11-16 16:25:30 -0500

    Refactor render.yaml to fix build errors

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -7,6 +7,7 @@ export default defineConfig({
   schema: "src/prisma/schema.prisma",
   migrations: {
     path: "src/prisma/migrations",
+    seed: "ts-node src/prisma/seed.ts",
   },
   engine: "classic",
   datasource: {
